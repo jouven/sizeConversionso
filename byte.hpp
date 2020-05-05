50157c75a97412817a01632440dbf94e5011d853
jouven <jouven@gmail.com>
2020-05-05 14:39:48 +0200

    Code refactoring

--- byte.hpp
+++ byte.hpp
@@ -6,7 +6,7 @@
 #include <string>
 
 //afterCommaSize from 0 to 4 (0-1024 range)
-std::string EXPIMP_SIZECONVERSIONSO formatByteSizeValue_f(const uint_fast64_t value_par, const int afterCommaSize = 1);
+EXPIMP_SIZECONVERSIONSO std::string formatByteSizeValue_f(const uint_fast64_t value_par, const int afterCommaSize = 1);
 
 
 #endif // SIZECONVERSIONSO_BYTE_HPP
